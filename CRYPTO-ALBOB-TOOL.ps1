c1265b5f185e95dcd8b6107d3d55044525ffa523
KenMcClean <65670906+KenMcClean@users.noreply.github.com>
2021-02-03 19:18:08 -0800

    Update CRYPTO-ALBOB-TOOL.ps1

--- CRYPTO-ALBOB-TOOL.ps1
+++ CRYPTO-ALBOB-TOOL.ps1
@@ -1,14 +1,11 @@
 clear
 function Get-Site
 {
-	$site=Read-Host "Enter E to encode, D to decode, or X to exit"
-	Switch ($site)
-	{
-		E {$input = read-host "Please enter a message to be encoded"
-
-$input = $input.ToString()
-$input = $input.toupper()
-#Take all of the characters and make them uppercase
+$site=Read-Host "Enter E to encode, D to decode, or X to exit"
+#By using a switch, we only give the user three choices.  That way, we don't need to do error handling
+Switch ($site)
+{
+E {$input = read-host "Please enter a message to be encoded"
 
 $charArray = $input.tochararray()
 #Add the characters to an array
@@ -16,7 +13,7 @@ $charArray = $input.tochararray()
 $day = get-date -format "dd"
 #Get today's date
 
-$factor =  230683 * $day
+ $multiple = 230683 * $day
 #multiply the secret by the date
 
 $ciphertext = ""
@@ -28,31 +25,32 @@ $charvalue = [int[]][char]$char
 
 foreach($letter in $charvalue){
 
-$value = $letter * $factor
+$value = $letter *  $multiple
 #multiply the ascii value of the letter by (secret * date)
 
 $value = $value.ToString() + " "
 #Turn the value into a string so we can add a space to it
 
 $ciphertext += $value
-}
-}
+}}
+$date = Get-Date
 write-host $ciphertext
+write-host "Message encoded on $date"
+
+#spit out C, the ciphertext
 }
-		D {$input = read-host "Please enter an encrypted value to be decoded"
-
+D {$input = read-host "Please enter an encrypted value to be decoded"
 $input = $input.split(" ")
-
+#Split the string of values when a space is encountered
 foreach($value in $input){
 
-$value = $value / $factor
-
+$value = $value /  $multiple
+#Divide the value by the multiple
 [Char][int]$value
-$x = 1
+#Turn the ASCII value back into a character
 }}
 X {Read-Host -Prompt "Press Enter to exit"}
 }}
 
 get-site
-
 
