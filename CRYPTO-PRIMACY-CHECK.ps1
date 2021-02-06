6bd8d5d0baae808f36ddc3779499f5cce1455c11
KenMcClean <65670906+KenMcClean@users.noreply.github.com>
2021-02-06 12:49:38 -0800

    Update CRYPTO-PRIMACY-CHECK.ps1

--- CRYPTO-PRIMACY-CHECK.ps1
+++ CRYPTO-PRIMACY-CHECK.ps1
@@ -1,48 +1,75 @@
+clear
 $x = 0
 
-$plainText = read-host "Please enter a message to be hashed"
+
+#---------Take the message as an input, and check to see if it's divisible by two. If it's divisible by two, it's not a prime number, and it's length should be offset by 1 unit
+$message = read-host "Please enter a message to be hashed"
+$mlen  = $message.length
+if(([int]$mlen / 2 ).GetType().Name -eq 'Int32'){
+$message = $message + "0"
+}
+#---------
+
+
+#---------Assume both the message and public key are prime, and test to confirm that
+$mLenPrime = $true
+$pubPrime = $true
+#---------
+
+
+#---------Get the public key, and check to see if it's prime
 $publicKey = read-host "Please enter a public key"
-#Get the message and the public key of the sender
 
-$isPrime = $true
-$plainTextLength = $plainText.length
-#Get the length of the message
+$publicKeyRange = 2..($publicKey-1)
+#The range starts at 2, because all prime numbers are divisible by 1
+#The range ends at the user-supplied value less one, so we're not trying to divide the value by itself
 
-$modulusInt = [int]$publicKey + [int]$plainTextLength
-#Get the result of the public key + the length of the plaintext message
-
-$range = 2..($modulusInt-1)
-#Get the range of numbers to be tested
-
-
-
-while($x -lt ($modulusInt-1)){
-#Loop until the number of loops equals the $modulusInt, minus one
-
-foreach($rangeInt in $range){
-$primeTestVar = $modulusInt / $rangeInt 
-
-#write-host "foreach test is" $primeTestVar
-
-if($primeTestVar.GetType().Name -eq 'Int32'){
-#write-host "if test is" $primeTestVar
-
-write-host "found a whole number" $primeTestVar
-$isPrime = $false
-
+foreach($publicKeyInt in $publicKeyRange){
+if(([int]$publicKey / $publicKeyInt ).GetType().Name -eq 'Int32'){
+#If any of the numbers between 2 and the public key divide by the public key into a whole number, then the public key is not prime, and we can set $pubPrime to false
+$pubPrime = $false
 }
-$x++
-
 }
-
+if($pubPrime -eq $false){
+write-host "The public key was not a prime number. Public keys must be prime numbers"
+break
 }
+#---------The public key has now been confirmed to be prime, or the user has been prompted to try again with a prime number
 
 
+#---------Set the same style of range, using the message length minus 1 as the maximum
+$messageRange = 2..($mlen-1)
+foreach($mLenInt in $messageRange){
 
-if($isPrime -eq $true){
+if(([int]$mlen / $mLenInt).GetType().Name -eq 'Int32'){
+$mLenPrime = $false
+#Again, if the length of the message divided by any of the numbers in the range is a whole number, then the message length is not a prime number
+}}
+#---------
 
-write-host "Prime is true"
+
+#---------So long as the message length is not prime, run this function
+while($mLenPrime -eq $false){
+
+$messageRange = 2..($message.length-1)
+
+foreach($int in $messageRange){
+
+if(([int]$message.length / $Int).GetType().Name -eq 'Int32'){
+#For each integer in the message length range, if the message length divided by the integer is whole, the length of the message is still not prime
+$mLenPrime = $false
+$message = $message +"0"
+#Therefor, we should pad the message with another character, and test it again to see if it's prime
+}else{
+#Otherwise the length must be prime. This only works because we're testing each integer in the range, and setting it to true or false
+$mLenPrime = $true
 }
+}
+}
+#---------
 
+
+write-host "Message has been padded to be" $message
+$message.Length
 
 
